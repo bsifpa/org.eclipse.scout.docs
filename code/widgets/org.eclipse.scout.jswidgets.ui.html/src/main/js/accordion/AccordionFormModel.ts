6b4bc81c29ae1af71bc157a14723a1124de7ca25
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2022-11-08 14:05:08 +0100

    AccordionForm migration to typescript (rename)

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/accordion/AccordionFormModel.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/accordion/AccordionFormModel.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
 import {Accordion, AccordionField, CheckBoxField, Group, GroupBox, ImageField, LabelField, Menu, SmartField, TabItem} from '@eclipse-scout/core';
 import {ConfigurationBox, EventsTab, FormFieldActionsBox, FormFieldPropertiesBox, GridDataBox, WidgetActionsBox} from '../index';
 
